e0153324ac4321c8f41952af04f774fde5823234
Mariejoe Diamzon <90502737+MariejoeD@users.noreply.github.com>
2025-01-15 21:56:08 +0800

    need help

--- Script/almanac.gd
+++ Script/almanac.gd
@@ -1,31 +1,64 @@
 extends Control
 
-# Preload or declare nodes
-@onready var enemy_container = $Panel/EnemyContainer
-@onready var ally_container = $Panel/AllyContainer
-@onready var back_button = $Panel/BackButton
-@onready var enemy_button = $Panel/enemyButton
-@onready var ally_button = $Panel/allyButton
+# Declare necessary nodes for buttons and containers
+@onready var back_button = $mainPanel/VBoxContainer/BackButton
+@onready var ally_button = $mainPanel/VBoxContainer/AllyButton
+@onready var enemy_button = $mainPanel/VBoxContainer/EnemyButton
+@onready var enemy_container = $mainPanel/picturePanel/EnemyContainer
+@onready var description_panel = $mainPanel/picturePanel/DescriptionPanel
 
+# Dictionary to store button-to-description mapping
+var descriptions = {
+	"Agta": $mainPanel/picturePanel/DescriptionPanel/AgtaDescription,
+	"Duwende": $mainPanel/picturePanel/DescriptionPanel/DuwendeDescription,
+	"Tikbalang": $mainPanel/picturePanel/DescriptionPanel/TikbalangDescription,
+	"Kapre": $mainPanel/picturePanel/DescriptionPanel/KapreDescription,
+	"Manananggal": $mainPanel/picturePanel/DescriptionPanel/MananaggalDescription,
+	"Sigbin": $mainPanel/picturePanel/DescriptionPanel/SigbinDescription,
+	"TinienteGimo": $mainPanel/picturePanel/DescriptionPanel/TinienteGimoDescription,
+	"Amomongo": $mainPanel/picturePanel/DescriptionPanel/AmomongoDescription,
+	"Mangkukulam": $mainPanel/picturePanel/DescriptionPanel/MangkukulamDescription,
+	"Lizardo": $mainPanel/picturePanel/DescriptionPanel/LizardoDescription,
+	# Add other descriptions here
+}
+
+# Hide containers and panels initially
 func _ready():
-	# Hide containers initially
 	enemy_container.visible = false
-	ally_container.visible = false
+	description_panel.visible = false
 
-	# Connect button signals
+	# Connect button signals using Callable
+	back_button.connect("pressed", Callable(self, "_on_back_button_pressed"))
+	ally_button.connect("pressed", Callable(self, "_on_ally_button_pressed"))
 	enemy_button.connect("pressed", Callable(self, "_on_enemy_button_pressed"))
-	ally_button.connect("pressed", Callable (self, "_on_ally_button_pressed"))
-	back_button.connect("pressed", Callable (self, "_on_back_button_pressed"))
 
-func _on_enemy_button_pressed():
-	# Open Enemy Container and hide Ally Container
-	enemy_container.visible = true
-	ally_container.visible = false
+	for name in descriptions.keys():
+		var button = $mainPanel/picturePanel/EnemyContainer/pictureContainer.get_node(name)
+		if button:
+			button.connect("pressed", Callable(self, "_on_character_button_pressed").bind(name))
 
-func _on_ally_button_pressed():
-	# Open Ally Container and hide Enemy Container
-	ally_container.visible = true
-	enemy_container.visible = false
-
+# Back button hides all panels
 func _on_back_button_pressed():
+	print("Back button pressed!")
 	get_tree().change_scene_to_file("res://Scene/MainMenu.tscn")
+
+# Ally button functionality (add as needed)
+func _on_ally_button_pressed():
+	print("Ally button clicked")
+	# Implement ally-related behavior here
+
+# Enemy button opens the enemy container
+func _on_enemy_button_pressed():
+	enemy_container.visible = true
+	description_panel.visible = false
+
+# Generic handler for character buttons
+func _on_character_button_pressed(name):
+	print(name + " button clicked")
+	description_panel.visible = true
+	
+	# Show the specific description
+	if descriptions.has(name):
+		print(descriptions.get(self.name))
+		descriptions[name].visible = true
+		
